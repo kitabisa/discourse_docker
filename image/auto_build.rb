01e372425834bb1867cb635406b26910dff765d2
Bherly Novrandy <pushm0v.development@gmail.com>
2023-04-14 23:52:27 +0700

    Add kasanak base

--- image/auto_build.rb
+++ image/auto_build.rb
@@ -4,7 +4,7 @@ require 'pty'
 require 'optparse'
 
 images = {
-  base: { name: 'base', tag: "discourse/base:build", squash: true },
+  base: { name: 'base', tag: "kasanak-discourse/base:build", squash: true },
   discourse_test_build: { name: 'discourse_test', tag: "discourse/discourse_test:build", squash: false},
   discourse_test_public: { name: 'discourse_test', tag: "discourse/discourse_test:release", squash: true, extra_args: ' --build-arg tag=release '},
   discourse_dev: { name: 'discourse_dev', tag: "discourse/discourse_dev:build", squash: false },
